25ceff3308a5a710991002bea67c62a91cb60199
volcodc <volcodc@gmail.com>
2015-02-26 00:55:04 -0600

    ember-cli-101
    ember-cli-101 project

--- app/models/friend.js
+++ app/models/friend.js
@@ -1,6 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  articles: DS.hasMany('article'),
   firstName: DS.attr('string'),
   lastName: DS.attr('string'),
   email: DS.attr('string'),
